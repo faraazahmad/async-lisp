8fbc26876de3483d8e58b2985f39bc8a0052c8f8
Syed Faraaz Ahmad <faraaz98@live.com>
2019-10-05 13:45:51 +0530

    reads lisp files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let file_name = &args[1];
+
+    let contents = fs::read_to_string(file_name)
+        .expect("Something went wrong reading the file.");
+    
+    println!("{:?}", contents);
 }
